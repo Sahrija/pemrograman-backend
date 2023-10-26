3d00ad731f328b14828823fc9f1c1f6f001309f9
Sahrija <ahmadsahrija@gmail.com>
2023-10-26 10:02:50 +0700

    edited tugas 3

--- Praktik-03/latihan-class.php
+++ Praktik-03/latihan-class.php
@@ -1,6 +1,9 @@
 <?php
 
-function newline(){echo "<br>";}
+function newline()
+{
+  echo "<br>";
+}
 
 class Animals
 {
@@ -19,18 +22,24 @@ class Animals
   public function store(string $newAnimal)
   {
     array_push($this->data, $newAnimal);
-    echo "add ". $newAnimal . "";
+    echo "add " . $newAnimal . "";
     newline();
   }
 
-  public function update(String $oldAnimal, String $newAnimal){
-    echo "update " . $oldAnimal ." to ". $newAnimal ."";
+  public function update(string $oldAnimal, string $newAnimal)
+  {
+    echo "update " . $oldAnimal . " to " . $newAnimal . "";
     newline();
     $old_animal_index = array_search($oldAnimal, $this->data);
+    if ($old_animal_index == false) {
+      echo 'animal "' . $oldAnimal . '" not found';
+      newline();
+    }
     $this->data[$old_animal_index] = $newAnimal;
   }
-  public function destroy($animal){
-    echo "delete ".$animal . "";
+  public function destroy($animal)
+  {
+    echo "delete " . $animal . "";
     newline();
     $animal_index = array_search($animal, $this->data);
     unset($this->data[$animal_index]);
@@ -42,7 +51,7 @@ $animals->store("cat");
 $animals->store("dog");
 $animals->store("chicken");
 $animals->index();
-$animals->update("dog", "dinosaur");
+$animals->update("do", "dinosaur");
 $animals->index();
 $animals->destroy("dinosaur");
 $animals->index();
