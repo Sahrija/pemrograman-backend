dbdbc4d862970aca686891bfb9085064fdefbb7c
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2023-12-01 12:56:22 +0700

    finished tugas 9

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-// console.log('djlf');
-// let user = {
-//   'nama': 'sahrija',
-//   'lapar': true,
-//   'makan' : function() {
-//     this.lapar = false;
-//   }
-// }
-
-// console.log(user);
-// console.log(user.nama);
-// console.log(user.lapar);
-// user.makan();
-// console.log(user.lapar);
-
-// let {nama, lapar} = user;
-// if(!lapar){console.log('turu');}
-
-// console.log(nama);
-
-/* 
-di padang rumput hijau yang jauh, terdapat seorang pengembala domba sedang 
-meneguk air dari botol peziarah, botol itu terbuat dari kulit binatang yang 
-telah diawetkan
-
-
-
-*/
-
 const {index, store, update, destroy} = require("./task/controller/fruitController.js");
 
 index();
