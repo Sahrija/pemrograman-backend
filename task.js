89585873d0082e757999d7becdbd424bc5fe0226
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2023-12-08 10:30:02 +0700

    finished task 10

--- task.js
+++ task.js
@@ -4,21 +4,25 @@
  */
 function showDownload(result) {
   console.log("Download selesai");
-  console.log("Hasil Download: " + result);
+  console.log(`Hasil Download: ${result}`);
 }
 
 /**
  * Fungsi untuk download file
  * @param {function} callback - Function callback show
  */
-function download(callShowDownload) {
-  setTimeout(function () {
-    const result = "windows-10.exe";
-    callShowDownload(result);
-  }, 3000);
+const download = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      const result = "windows-10.exe";
+      resolve(result);
+    }, 3000);
+  });
 }
 
-download(showDownload);
+download().then((result) => {
+  return showDownload(result)
+});
 
 /**
  * TODO:
